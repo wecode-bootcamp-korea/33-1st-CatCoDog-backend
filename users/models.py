be4cdf8e58498dd99bf0116ef60ab825285bf572
박준형 <>
2022-05-24 22:46:30 +0900

    signup view add

--- users/models.py
+++ users/models.py
@@ -1,22 +1,5 @@
 from django.db import models
-
-class User(models.Model):
-    name = models.CharField(max_length = 50)
-    email = models.CharField(max_length = 100, unique = True)
-    password = models.CharField(max_length = 200)
-    mobile_number = models.CharField(max_length=20)
-    address = models.CharField(max_length=200)
-    email_subscription = models.BooleanField()
-    membership_point = models.PositiveIntegerField()
-    pet_type = models.ForeignKey('PetType', on_delete = models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add = True)
-    updated_at = models.DateTimeField(auto_now = True)
-
-    class Meta:
-        db_table = 'users'
-
-    def __str__(self):
-        return self.name 
+from core.models import TimeStampedModel
 
 class PetType(models.Model):
     name = models.CharField(max_length = 20)
@@ -26,3 +9,21 @@ class PetType(models.Model):
 
     def __str__(self):
         return self.name 
+
+class User(TimeStampedModel):
+    name = models.CharField(max_length = 50)
+    email = models.CharField(max_length = 100, unique = True)
+    password = models.CharField(max_length = 200)
+    mobile_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=200)
+    email_subscription = models.BooleanField() 
+    membership_point = models.PositiveIntegerField()
+    pet_type = models.ForeignKey(PetType, on_delete = models.CASCADE)
+
+    class Meta:
+        db_table = 'users'
+
+    def __str__(self):
+        return self.name 
+
+
