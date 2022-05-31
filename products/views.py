cface793fe2425566816ed62e92a7a7bb4683e32
strzero <restouri@gmail.com>
2022-05-31 15:40:29 +0900

    코드리뷰 반영

--- products/views.py
+++ products/views.py
@@ -1,60 +1,64 @@
-from django.http            import JsonResponse
-from django.views           import View
-from django.db.models       import Q, Count, Sum, IntegerField, OuterRef, Subquery
-from django.core.exceptions import FieldError
+from enum import Enum
+
+from django.http      import JsonResponse
+from django.views     import View
+from django.db.models import Q, Count, Sum, IntegerField, OuterRef, Subquery
 
 from products.models import Product
 
+class ProductOption(Enum):
+    ONE = "1개입"
+    TEN = "10개입"
+
 class ProductListView(View):
     def get(self, request):
-        try:
-            category = request.GET.get('category', None)
-            search   = request.GET.get('search', None)
-            sort     = request.GET.get('sort', 'name')
-            offset   = int(request.GET.get('offset', 0))
-            limit    = int(request.GET.get('limit', 6))
 
-            q = Q()
+        category = request.GET.get('category', None)
+        search   = request.GET.get('search', None)
+        sort     = request.GET.get('sort', 'name')
+        offset   = int(request.GET.get('offset', 0))
+        limit    = int(request.GET.get('limit', 6))
 
-            q &= Q(options__name="1개입")
+        q = Q()
 
-            if category:
-                q &= Q(category__name__contains=category)
+        q &= Q(options__name=ProductOption.ONE.value)
 
-            if search:
-                q &= Q(name__contains=search)
-            
-            sort_dict = {
-                "new"   : "-created_at",
-                "old"   : "created_at",
-                "price" : "options__price",
-                "-price": "-options__price",
-                "review": "-review_count",
-                "sales" : "-sales_sum",
-            }
+        if category:
+            q &= Q(category__name__contains=category)
 
-            if sort in sort_dict:
-                sort = sort_dict[sort]
+        if search:
+            q &= Q(name__contains=search)
 
-            review_count = Product.objects.annotate(review_count=Count('reviews')).filter(pk=OuterRef('pk'))
-            sales_sum    = Product.objects.annotate(sales_sum=Sum('options__items__quantity')).filter(pk=OuterRef('pk'))
+        sort_set = {
+            "name"  : "name",
+            "-name" : "-name",
+            "old"   : "created_at",
+            "new"   : "-created_at",
+            "price" : "options__price",
+            "-price": "-options__price",
+            "review": "-review_count",
+            "sales" : "-sales_sum",            
+        }
 
-            products     = Product.objects.filter(q).annotate(review_count=Subquery(review_count.values('review_count'), 
-            output_field=IntegerField()),sales_sum=Subquery(sales_sum.values('sales_sum'), output_field=IntegerField()))\
-            .order_by(sort).distinct()[offset:offset+limit]
-            
-            product_list = [{
-                "product_id"      : product.id,
-                "name"            : product.name,
-                "description"     : product.description,
-                "thumbnail_url"   : product.thumbnail_url,
-                "review_count"    : product.reviews.count(),
-                "price"           : product.options.first().price,
-                "discount_rate"   : product.discount_rate,
-                "discounted_price": product.options.first().price * (100 - product.discount_rate)/100
-            } for product in products]
+        sort = sort_set.get(sort)
 
-            return JsonResponse({"data": product_list}, status=200)
-        
-        except FieldError:
-            return JsonResponse({"message": "FIELD_ERROR"}, status=400)
+        review_count = Product.objects.annotate(review_count=Count('reviews')).filter(pk=OuterRef('pk'))
+        sales_sum    = Product.objects.annotate(sales_sum=Sum('options__items__quantity')).filter(pk=OuterRef('pk'))
+
+        products     = Product.objects.filter(q).annotate\
+                            (review_count=Subquery(review_count.values('review_count'), output_field=IntegerField()),\
+                            sales_sum=Subquery(sales_sum.values('sales_sum'), output_field=IntegerField()))\
+                            .order_by(sort).distinct()[offset:offset+limit]
+
+        product_list = [{
+            "product_id"      : product.id,
+            "name"            : product.name,
+            "description"     : product.description,
+            "thumbnail_url"   : product.thumbnail_url,
+            "review_count"    : product.reviews.count(),
+            "price"           : product.options.first().price,
+            "discount_rate"   : product.discount_rate,
+            "discounted_price": product.options.first().price * (100 - product.discount_rate)/100,
+        } for product in products]
+
+        return JsonResponse({"data": product_list}, status=200)
