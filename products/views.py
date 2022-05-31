322e546ebc5ad79fcc4117caa91f89e84d5b433f
박준형 <>
2022-05-31 14:28:59 +0900

    edit product_detail/Views

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
-from django.http     import JsonResponse
-from django.views    import View
+from django.http  import JsonResponse
+from django.views import View
 
-from products.models import Product, ProductImage, ProductOption
+from products.models import Product
 
 class ProductDetailView(View):
     def get(self, request, product_id):
@@ -15,15 +15,16 @@ class ProductDetailView(View):
                 'product_image'   : [image.url for image in product.image_url.all()],
                 'description'     : product.description,
                 'discount_rate'   : product.discount_rate,
-                'price'           : [option.price for option in product.options.all()],
-                'discounted_price': [int(option.price)* (100 - product.discount_rate)/100 for option in product.options.all()],
-                'product_option'  : [option.name for option in product.options.all()]
+                'product_option'  : [{
+                    "option_name"     : option.name,
+                    "optios_price"    : option.price,
+                    "discounted_price": int(option.price) * (100 - product.discount_rate)/100
+                    } for option in product.options.all()]
             }
-            
             return JsonResponse({'result' : data}, status=200)
 
         except KeyError:
                 return JsonResponse({'message' : 'Key Error'}, status=400)
 
         except Product.DoesNotExist:
-                return JsonResponse({'message' : 'NOT_FOUND'}, status=401)
+                return JsonResponse({'message' : 'NOT_FOUND'}, status=404)
