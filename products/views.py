166786e248d45e9cf1875eb7bf2f4008171a8238
strzero <restouri@gmail.com>
2022-06-01 17:31:01 +0900

    conflict 수정

--- products/views.py
+++ products/views.py
@@ -59,3 +59,29 @@ class ProductListView(View):
         } for product in products]
 
         return JsonResponse({"data": product_list}, status=200)
+
+class ProductDetailView(View):
+    def get(self, request, product_id):
+        try:
+            
+            product = Product.objects.get(id = product_id) 
+            data = {
+                'id'              : product.id,
+                'name'            : product.name,
+                'thumbnail_url'   : product.thumbnail_url,
+                'product_image'   : [image.url for image in product.image_url.all()],
+                'description'     : product.description,
+                'discount_rate'   : product.discount_rate,
+                'product_option'  : [{
+                    "option_name"     : option.name,
+                    "optios_price"    : option.price,
+                    "discounted_price": int(option.price) * (100 - product.discount_rate)/100
+                    } for option in product.options.all()]
+            }
+            return JsonResponse({'result' : data}, status=200)
+
+        except KeyError:
+                return JsonResponse({'message' : 'Key Error'}, status=400)
+
+        except Product.DoesNotExist:
+                return JsonResponse({'message' : 'NOT_FOUND'}, status=404)
